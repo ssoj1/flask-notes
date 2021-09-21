0da3855127f229556288cdfc908fecf38e22d471
SARA JENSEN <SSOJENSEN@GMAIL.COM>
2021-09-21 12:31:43 -0700

    adding User model, base and register form HTML, and / and /register routes

---
+++ app.py
@@ -0,0 +1,49 @@
+"""Flask app for Notes"""
+
+from flask import Flask, jsonify, request, render_template, redirect
+from flask_debugtoolbar import DebugToolbarExtension
+from models import User, db, connect_db
+import os
+
+app = Flask(__name__)
+
+API_SECRET_KEY=os.environ['API_SECRET_KEY']
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///users"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+db.create_all()
+
+@app.get("/")
+def redirect_to_register():
+    """Redirect to /register"""
+
+    return redirect("/register")
+
+@app.route("/register", method=["GET" , "POST"])
+def register_user():
+    """ """
+
+    form = User()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+
+        new_user = User(username=username, 
+                        password=password, 
+                        email=email, 
+                        first_name=first_name, 
+                        last_name=last_name)
+
+                        # dont forget to hash password!
+
+
+
+    return render_template("register_form.html")
+
---
+++ forms.py
@@ -0,0 +1,31 @@
+"""Forms for List app."""
+
+from wtforms.fields.simple import PasswordField
+from wtforms.validators import InputRequired, Optional, Email, Length, DataRequired
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, TextAreaField, BooleanField
+
+class RegisterForm(FlaskForm):
+    """Register a new user"""
+
+    username = StringField("Username", 
+                            validators=[InputRequired(), 
+                            Length(max=20)
+                            ])
+    password = PasswordField("Password", 
+                            validators=[DataRequired(), 
+                            Length(max=100)
+                            ])
+    email = StringField("Email Address", 
+                            validators=[InputRequired(), 
+                            Email(), 
+                            Length(max=50)
+                            ])
+    first_name = StringField("First Name", 
+                            validators=[InputRequired(), 
+                            Length(max=30)
+                            ])
+    last_name = StringField("Last Name", 
+                            validators=[InputRequired(), 
+                            Length(max=30)
+                            ])
